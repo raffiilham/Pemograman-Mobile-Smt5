920ff8a17988ff67d902760b6953fe91d937987b
raffiilham <raffiilham47@gmail.com>
2024-09-26 19:18:18 +0700

    final

--- #04_PengantarBahasaPemrogramanDart-Bagian 3/main2.dart
+++ #04_PengantarBahasaPemrogramanDart-Bagian 3/main2.dart
@@ -1,26 +1,19 @@
 void main() {
-  // Langkah 1 - Set dengan elemen halogen
-  var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astantine'};
-  print(halogens);
+  // // Langkah 1 - Set dengan elemen halogen
+  // var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astantine'};
+  // print(halogens);
 
   // Langkah 3
-  var names1 = <String>{}; // Set kosong
-  Set<String> names2 = {}; // Set kosong
-  var names3 = <String, String>{}; // Map kosong
+var names1 = <String>{};
+Set<String> names2 = {}; // This works, too.
+var names3 = {}; // Creates a map, not a set.
 
-  // Menambahkan elemen nama dan NIM menggunakan .add() pada names1
-  names1.add("Raffi Ilham Maulana"); // Nama
-  names1.add("2241720226");          // NIM
+names1.add('Raffi Ilham Maulana');
+names1.add('2241720226');
 
-  // Menambahkan elemen nama dan NIM menggunakan .addAll() pada names2
-  names2.addAll({"Raffi Ilham Maulana", "2241720226"});
+names2.addAll({'Raffi Ilham Maulana', '2241720226'});
 
-  // Menambahkan elemen ke map names3
-  names3['nama'] = "Raffi Ilham Maulana";
-  names3['nim'] = "2241720226";
-
-  // Print hasil
-  print(names1); // Set dengan nama dan NIM
-  print(names2); // Set dengan nama dan NIM
-  print(names3); // Map dengan nama dan NIM
+print(names1);
+print(names2);
+print(names3);
 }
