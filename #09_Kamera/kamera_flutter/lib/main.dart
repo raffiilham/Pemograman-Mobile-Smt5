324f48f175c7f336b67b8a03f34075b3d99eb7ae
raffiilham <raffiilham47@gmail.com>
2024-10-31 15:09:56 +0700

    praktikum 1 staged - implemented camera

--- #09_Kamera/kamera_flutter/lib/main.dart
+++ #09_Kamera/kamera_flutter/lib/main.dart
@@ -1,20 +1,25 @@
-import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-
-List<CameraDescription> cameras = [];
+import 'package:flutter/material.dart';
+import 'package:kamera_flutter/widget/takepicture_screen.dart';
 
 Future<void> main() async {
-  // Pastikan plugin diinisialisasi sebelum memanggil `availableCameras()`
+  // Ensure that plugin services are initialized so that availableCameras()
+  // can be called before runApp()
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Ambil daftar kamera yang tersedia pada perangkat
-  cameras = await availableCameras();
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
 
-  runApp(const MyApp());
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+
+  runApp(MyApp(firstCamera: firstCamera));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.firstCamera});
+
+  final CameraDescription firstCamera;
 
   @override
   Widget build(BuildContext context) {
@@ -24,19 +29,16 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(
-        title: 'Flutter Demo Home Page',
-        camera: cameras.isNotEmpty ? cameras.first : null, // Pasangkan kamera pertama
-      ),
+      debugShowCheckedModeBanner: false,
+      home: TakePictureScreen(camera: firstCamera), // Pass the camera here
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  final String title;
-  final CameraDescription? camera;
+  const MyHomePage({super.key, required this.title});
 
-  const MyHomePage({super.key, required this.title, this.camera});
+  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -59,11 +61,18 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Text('You have pushed the button this many times:'),
-          Text('$_counter', style: Theme.of(context).textTheme.headlineMedium),
-        ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
---
+++ #09_Kamera/kamera_flutter/lib/widget/takepicture_screen.dart
@@ -0,0 +1,91 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:kamera_flutter/widget/displaypicture_screen.dart';
+
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({
+    super.key,
+    required this.camera,
+  });
+
+  final CameraDescription camera;
+
+  @override
+  TakePictureScreenState createState() => TakePictureScreenState();
+}
+
+class TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CameraController(
+      widget.camera,
+      ResolutionPreset.medium,
+    );
+
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Take a picture')),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Column(
+              children: [
+                // Display the camera preview
+                Expanded(child: CameraPreview(_controller)),
+              ],
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        // Provide an onPressed callback.
+        onPressed: () async {
+          // Take the Picture in a try / catch block. If anything goes wrong,
+          // catch the error.
+          try {
+            // Ensure that the camera is initialized.
+            await _initializeControllerFuture;
+
+            // Attempt to take a picture and get the file path
+            final image = await _controller.takePicture();
+
+            if (!context.mounted) return;
+
+            // If the picture was taken, display it on a new screen.
+            await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(
+                  // Pass the automatically generated path to
+                  // the DisplayPictureScreen widget.
+                  imagePath: image.path,
+                ),
+              ),
+            );
+          } catch (e) {
+            // If an error occurs, log the error to the console.
+            print(e);
+          }
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
+    );
+  }
+}
